9bc6119dac124237c214be3066c9a4498bdabfe5
Lorenzo Wormer <l.wormer@pm.me>
2023-12-09 00:10:46 +0100

    ✨ new reflection features

--- src/py_research/reflect.py
+++ src/py_research/reflect.py
@@ -1,8 +1,19 @@
 """Utils for Python code reflection."""
 
 import inspect
-from collections.abc import Callable
-from typing import Any, Sequence, TypeVar
+from collections.abc import Callable, Sequence
+from dataclasses import dataclass
+from functools import cache, reduce
+from importlib import import_module
+from pathlib import Path
+from types import ModuleType
+from typing import Any, Generic, TypeVar
+
+import importlib_metadata as meta
+from git import Repo
+
+T = TypeVar("T")
+T2 = TypeVar("T2")
 
 
 def _get_calling_frame(offset=0):
@@ -32,7 +43,14 @@ def get_full_args_dict(
     return {**kwdefaults, **posargs, **(kwargs or {})}
 
 
-T = TypeVar("T")
+def get_return_type(func: Callable) -> type | None:
+    """Get the return type annotation of given function, if any."""
+    sig = inspect.signature(func)
+    return (
+        sig.return_annotation
+        if sig.return_annotation != inspect.Signature.empty
+        else None
+    )
 
 
 def get_all_subclasses(cls: type[T]) -> set[type[T]]:
@@ -40,3 +58,159 @@ def get_all_subclasses(cls: type[T]) -> set[type[T]]:
     return set(cls.__subclasses__()).union(
         [s for c in cls.__subclasses__() for s in get_all_subclasses(c)]
     )
+
+
+@cache
+def _get_distributions() -> dict[str, meta.Distribution]:
+    return {d.metadata["Name"]: d for d in meta.distributions()}
+
+
+def _get_module_file(module: ModuleType) -> Path | None:
+    return (
+        Path(str(module.__file__))
+        if not hasattr(module, "__file__") or module.__file__ is None
+        else None
+    )
+
+
+def get_module_distribution(module: ModuleType) -> meta.Distribution | None:
+    """Get the distribution package of given module, if any."""
+    mod_file = _get_module_file(module)
+    if mod_file is None:
+        return None
+
+    dists = {
+        Path(dist.locate_file(f"{name}")): dist
+        for name, dist in _get_distributions().items()
+    }
+
+    mod_dists = [
+        dist for dist_path, dist in dists.items() if mod_file.is_relative_to(dist_path)
+    ]
+
+    return mod_dists[0] if len(mod_dists) > 0 else None
+
+
+def get_all_module_dependencies(
+    module: ModuleType,
+    ext_deps: set[str] | None = None,
+    int_deps: set[ModuleType] | None = None,
+) -> tuple[set[str], set[ModuleType]]:
+    """Return all dependency modules of given module."""
+    if ext_deps is None:
+        ext_deps = set()
+    if int_deps is None:
+        int_deps = set()
+
+    deps = [
+        dep
+        for _, m in inspect.getmembers(module)
+        if (dep := inspect.getmodule(m)) is not None
+    ]
+    ext_deps_map = {
+        dep: dist.metadata["Name"]
+        for dep in deps
+        if (dist := get_module_distribution(dep)) is not None
+    }
+    new_ext_deps = set(ext_deps_map.values()) - ext_deps
+
+    these_int_deps = set(deps) - set(ext_deps_map.keys())
+    new_int_deps = these_int_deps - int_deps
+
+    if len(new_ext_deps) > 0 or len(new_int_deps) > 0:
+        ext_deps |= new_ext_deps
+        int_deps |= new_int_deps
+
+        sub_ext_deps, sub_int_deps = zip(
+            *[get_all_module_dependencies(d, ext_deps, int_deps) for d in new_int_deps]
+        )
+
+        return set.union(*sub_ext_deps), set.union(*sub_int_deps)
+    else:
+        return ext_deps, int_deps
+
+
+def get_module_repo(module: ModuleType) -> Repo | None:
+    """Get the Git repository of given module, if any."""
+    mod_file = _get_module_file(module)
+    if mod_file is None:
+        return None
+
+    return Repo(mod_file)
+
+
+@dataclass
+class PyObjectRef(Generic[T]):
+    """Reference to a static Python object."""
+
+    type: type[T]
+    package: str
+    module: str
+    object: str
+    version: str = "1.0.0"
+    url: str = "https://pypi.org"
+
+    @staticmethod
+    def _get_pkg_url(module: ModuleType, dist: meta.Distribution) -> str | None:
+        url: str | None = (
+            dict(dist.origin).get("url") if dist.origin is not None else None
+        )
+
+        if url is not None and url.startswith("file://"):
+            repo = get_module_repo(module)
+            if repo is not None:
+                url = repo.remote().url
+
+        return url
+
+    @staticmethod
+    def reference(obj: T2, version: str | None = None) -> "PyObjectRef[T2]":
+        """Create a reference to given object."""
+        qualname = getattr(obj, "__qualname__")
+        if qualname is None:
+            raise ValueError("Object must have fully qualified name (`__qualname__`)")
+
+        module = inspect.getmodule(obj)
+        if module is None:
+            raise ValueError("Object must be associated with a module.")
+
+        dist = get_module_distribution(module)
+        if dist is None:
+            raise ValueError("Object's module ")
+
+        url = PyObjectRef._get_pkg_url(module, dist)
+
+        return PyObjectRef(
+            type(obj),
+            dist.name,
+            module.__name__,
+            qualname,
+            **(dict(version=version) if version else {}),
+            **(dict(url=url) if url else {}),
+        )
+
+    def resolve(self) -> T:
+        """Resolve object reference."""
+        dist = _get_distributions().get(self.package)
+        if dist is None:
+            raise ImportError(
+                f"Please install package '{self.package}' "
+                f"from '{self.url}' to resolve this object reference."
+            )
+
+        module = import_module(".".join([self.package, self.module]))
+        url = PyObjectRef._get_pkg_url(module, dist)
+        if url is not None and url != self.url:
+            raise ImportError(
+                f"URL mismatch: Package '{self.package} should be from "
+                f"'{self.url}' but is from '{url}'."
+            )
+
+        obj = reduce(getattr, self.object.split("."), module)
+        if not isinstance(obj, self.type):
+            raise TypeError(
+                f"Object `{'.'.join([self.package, self.module, self.object])}` "
+                f"must have type `{self.type}`"
+            )
+
+        return obj
