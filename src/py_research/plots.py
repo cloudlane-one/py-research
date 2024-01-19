68e4274f96075b86ac1239fd228bb1fd1075b8e8
Lorenzo Wormer <l.wormer@pm.me>
2024-01-19 20:53:00 +0100

    ✨(plots): decorator to add dropdown to plotly figure

--- src/py_research/plots.py
+++ src/py_research/plots.py
@@ -1,15 +1,18 @@
 """Utilities for working with common plots."""
 
+from collections.abc import Callable, Mapping
+from functools import reduce, wraps
 from pathlib import Path
-from typing import Literal, TextIO, TypeAlias
+from typing import Any, Concatenate, Literal, ParamSpec, TextIO, TypeAlias, cast
 
-from plotly.graph_objects import Figure as PlotlyFigure
+import pandas as pd
+import plotly.graph_objects as go
 
 ImageFormat: TypeAlias = Literal["svg", "pdf", "png", "jpg", "webp"]
 
 
 def export_html(
-    fig: PlotlyFigure,
+    fig: go.Figure,
     out: Path | str | TextIO,
     width: int = 800,
     height: int = 450,
@@ -43,3 +46,137 @@ def export_html(
             },
         },
     )
+
+
+P = ParamSpec("P")
+PlottingFunction = Callable[Concatenate[pd.DataFrame, P], go.Figure]
+
+
+def _merge_data(figs: Mapping[str, go.Figure | go.Frame]) -> list:
+    return [d for fig in figs.values() for d in (fig.data or [])]
+
+
+def _merge_layout(figs: Mapping[str, go.Figure | go.Frame]) -> dict[str, Any]:
+    return reduce(
+        lambda a, b: {**a, **b},
+        [
+            cast(dict, fig.layout.to_plotly_json())  # type: ignore
+            for fig in figs.values()
+        ],
+    )
+
+
+def _get_frames(figs: Mapping[str, go.Figure], label: str) -> dict[str, go.Frame]:
+    return {
+        group: frame
+        for group, fig in figs.items()
+        for f in fig.frames
+        if (frame := cast(go.Frame, f)).name == label
+    }
+
+
+go.Frame
+
+
+def with_dropdown(
+    group_by: str,
+    dropdown_kwars: dict[str, Any] | None = None,
+) -> Callable[[PlottingFunction[P]], PlottingFunction[P]]:
+    """Add a dropdown to a plotting function.
+
+    Args:
+        group_by: Column to group by for the dropdown.
+        dropdown_kwars: Keyword arguments to pass to the dropdown layout.
+
+    Returns:
+        Decorator that adds a dropdown to the returned plot.
+    """
+
+    def decorator(func: PlottingFunction[P]) -> PlottingFunction[P]:
+        """Add a dropdown to a plotting function."""
+
+        @wraps(func)
+        def wrapper(data: pd.DataFrame, *args: P.args, **kwargs: P.kwargs) -> go.Figure:
+            figs = {
+                str(group): func(sub_data, *args, **kwargs)
+                for group, sub_data in data.groupby(group_by)
+            }
+
+            fig_data = _merge_data(figs)
+            layout = _merge_layout(figs)
+
+            frames = None
+            if "sliders" in layout:
+                labels = [step["label"] for step in layout["sliders"][0]["steps"]]
+                frames = []
+                for label in labels:
+                    sub_frames = _get_frames(figs, label)
+                    frames.append(
+                        go.Frame(
+                            data=_merge_data(sub_frames),
+                            layout=_merge_layout(sub_frames),
+                            name=label,
+                        )
+                    )
+
+            split_fig = go.Figure(
+                data=fig_data,
+                layout=layout,
+                frames=frames,
+            )
+
+            trace_sources = pd.Series(
+                [group for group, fig in figs.items() for _ in fig.data]
+            )
+
+            split_fig.update_layout(
+                updatemenus=[
+                    *(
+                        [split_fig.layout["updatemenus"][0]]  # type: ignore
+                        if "updatemenus" in layout
+                        else []
+                    ),
+                    {
+                        "active": 0,
+                        "buttons": [
+                            {
+                                "label": "All",
+                                "method": "update",
+                                "args": [
+                                    {
+                                        "visible": [True] * len(trace_sources),
+                                    },
+                                ],
+                            },
+                            *(
+                                {
+                                    "label": group,
+                                    "method": "update",
+                                    "args": [
+                                        {
+                                            "visible": (
+                                                trace_sources == str(group)
+                                            ).to_list(),
+                                        },
+                                    ],
+                                }
+                                for group in figs.keys()
+                            ),
+                        ],
+                        "direction": "up",
+                        "pad": {"r": 10, "t": 70},
+                        "showactive": True,
+                        "x": 1.1,
+                        "xanchor": "left",
+                        "y": 0,
+                        "yanchor": "top",
+                        **(dropdown_kwars or {}),
+                    },
+                ]
+            )
+
+            return split_fig
+
+        return wrapper
+
+    return decorator
--- tests/py_research/test_plots.py
+++ tests/py_research/test_plots.py
@@ -3,14 +3,17 @@
 from pathlib import Path
 from tempfile import gettempdir
 
-from plotly.graph_objects import Figure
-from py_research.plots import export_html
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+from py_research.plots import export_html, with_dropdown
 
 
 def test_export_html():
     """Test export_html."""
     # Create a simple figure
-    fig = Figure(data=[{"type": "scatter", "x": [1, 2, 3], "y": [4, 5, 6]}])
+    fig = go.Figure(data=[{"type": "scatter", "x": [1, 2, 3], "y": [4, 5, 6]}])
 
     # Test with a Path object
     path = Path(gettempdir()) / "test_file.html"
@@ -19,3 +22,24 @@ def test_export_html():
         assert path.is_file()
     finally:
         path.unlink()  # Clean up
+
+
+def plot_func(df: pd.DataFrame) -> go.Figure:
+    """Plot a scatter plot of GDP per capita vs population with a slider for year."""
+    return px.scatter(
+        df,
+        x="pop",
+        y="gdpPercap",
+        color="country",
+        size="lifeExp",
+        animation_frame="year",
+    )
+
+
+def test_with_dropdown():
+    """Test ``with_dropdown`` decorator.""" ""
+    df = px.data.gapminder()
+    plot_func_wd = with_dropdown("continent")(plot_func)
+
+    fig = plot_func_wd(df)
+    assert isinstance(fig, go.Figure)
