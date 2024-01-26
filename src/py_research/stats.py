91b73278b9316e719791b57053184356fafbb553
Lorenzo Wormer <l.wormer@pm.me>
2024-01-26 01:57:23 +0100

    ✨(stats): value_col and domains for dist_table

--- src/py_research/stats.py
+++ src/py_research/stats.py
@@ -1,10 +1,17 @@
 """Helper functions for statistical evalutation of (dataframe-based) data."""
 
+from collections.abc import Hashable
+
+import numpy as np
 import pandas as pd
 
 
 def dist_table(
-    df: pd.DataFrame, id_cols: str | list[str] | None, category_cols: str | list[str]
+    df: pd.DataFrame,
+    category_cols: str | list[str],
+    id_cols: str | list[str] | None = None,
+    value_col: str | None = None,
+    domains: dict[str, list[Hashable] | np.ndarray | pd.Index] = {},
 ) -> pd.Series:
     """Return a frequency table of the distribution of unique entities.
 
@@ -13,11 +20,16 @@ def dist_table(
 
     Args:
         df: Dataframe to evaluate.
-        id_cols: Columns to identify entities by.
         category_cols: Columns to evaluate distribution over.
+        id_cols: Columns to identify entities by.
+        value_col: Unique values per entity to sum up.
+        domains:
+            Force the distribution to be evaluated over these domains,
+            filling missing values with 0.
 
     Returns:
-        Frequency table of the distribution of unique entities.
+        Series of the distribution's values (count or sum) given the categories
+        in the index.
     """
     id_cols = (
         [id_cols]
@@ -31,10 +43,51 @@ def dist_table(
     ]
 
     counts = (
-        df.reset_index()[[*category_cols, *id_cols]]
+        df.reset_index()[
+            [*category_cols, *id_cols, *([value_col] if value_col else [])]
+        ]
         .groupby(by=category_cols, group_keys=True)
-        .apply(lambda df: len(df.drop_duplicates()))
-        .rename("freq")
+        .apply(
+            lambda df: len(df.drop_duplicates())
+            if value_col is None
+            else df.drop_duplicates(subset=id_cols)[value_col].sum()
+        )
+        .rename("value")
     )
+
+    if len(domains) > 0:
+        if len(category_cols) == 1:
+            col = category_cols[0]
+            domain = domains.get(col)
+
+            if domain is not None:
+                counts = counts.reindex(
+                    domain,
+                    fill_value=0,
+                ).rename_axis(index=col)
+        else:
+            count_df = counts.to_frame().reset_index()
+
+            for col, domain in domains.items():
+                assert col in category_cols, f"Unknown category column: {col}"
+                other_cat = set(category_cols) - {col}
+
+                count_df = (
+                    count_df.groupby(list(other_cat), group_keys=True)
+                    .apply(
+                        lambda df: df.set_index(col)
+                        .drop(columns=[*list(other_cat)])
+                        .reindex(
+                            domain,
+                            fill_value=0,
+                        )
+                        .rename_axis(index=col)
+                        .reset_index()
+                    )
+                    .droplevel(1)
+                    .reset_index()
+                )
+
+            counts = count_df.set_index(category_cols)["value"]
 
     return counts
