361f51f19919b45f57df2998f01896289320003e
Lorenzo Wormer <l.wormer@pm.me>
2024-01-27 23:41:02 +0100

    ✨(plots): better responsive export

--- src/py_research/plots/__init__.py
+++ src/py_research/plots/__init__.py
@@ -3,7 +3,7 @@
 from collections.abc import Callable, Mapping
 from functools import reduce, wraps
 from pathlib import Path
-from textwrap import indent
+from textwrap import dedent, indent
 from typing import Any, Concatenate, Literal, ParamSpec, TextIO, TypeAlias, cast
 from uuid import uuid4
 
@@ -43,26 +43,21 @@ def export_html(
         f.write(
             plotly_to_html(
                 fig=fig,
-                default_width=width,
-                default_height=height,
                 download_scale=scale,
                 download_format=download_format,
             )
         )
 
 
-ResponsiveMode: TypeAlias = Literal[
-    None, "scale-image", "scale-layout", "stretch-width"
-]
+ResponsiveMode: TypeAlias = Literal[None, "keep-aspect", "stretch-width"]
 
 default_responsiveness: dict[ResponsiveMode, int | None] = {
-    None: 240,
-    "scale-image": 640,
+    None: 400,
+    "keep-aspect": 720,
     "stretch-width": 1080,
-    "scale-layout": 2160,
 }
 
-with open(Path(__file__).parent / "plotly_responsive.js") as f:
+with open(Path(__file__).parent / "responsive_plotly.js") as f:
     plotly_responsive_js: str = f.read()
 
 
@@ -73,44 +68,25 @@ def _ind(text: str, count: int) -> str:
 
 def plotly_to_html(
     fig: go.Figure,
-    resizing: dict[ResponsiveMode, int | None] | None = default_responsiveness,
-    default_width: int = 720,
-    default_height: int = 540,
+    write_to: Path | str | TextIO | None = None,
+    responsive: bool = True,
+    min_width: int = 540,
+    max_width: int = 1080,
+    max_height: int = 540,
     download_format: ImageFormat = "svg",
     download_scale: float = 3,
     full_html: bool = True,
-    plotly_js_url: str = "https://cdn.jsdelivr.net/npm/plotly.js@2/+esm",
+    plotly_js_url: str = "https://cdn.jsdelivr.net/npm/plotly.js@2/dist/plotly.min.js",
 ) -> str:
     """Convert plotly figure to interactive (and responsive) html.
 
     Args:
         fig: Plotly figure to save.
-        resizing:
-            Responsive sizing modes to use.
-            Accepts a dict mapping responsive modes to max widths in pixels.
-            A mode is used up until that width, and then it switches to next
-            or defaults to no responsiveness if no more modes are available.
-            Responsiveness can be disabled by setting this to ``None``.
-            Available modes are:
-                - ``None``: No responsiveness.
-                - ``"scale-image"``:
-                    Scale everything to fit the container,
-                    meaning text and markers will be scaled as well.
-                    Aspect ratio is maintained.
-                - ``"stretch-width"``:
-                    Increase the figure width to fit the container.
-                    Text and markers will not be scaled.
-                    Aspect ratio is not maintained.
-                - ``"scale-layout"``:
-                    Scale the layout to fit the container.
-                    Text and markers will not be scaled.
-                    Aspect ratio is maintained.
-        default_width:
-            Default width of the figure in pixels.
-            Used if responsiveness is disabled or when downloading an image.
-        default_height:
-            Default height of the figure in pixels.
-            Used if responsiveness is disabled or when downloading an image.
+        write_to: Path to save to or file-like object to write to.
+        responsive: Whether to make the figure responsive.
+        min_width: Minimum width of the figure in pixels.
+        max_width: Maximum width of the figure in pixels.
+        max_height: Maximum height of the figure in pixels.
         download_format: Format to use when downloading the figure.
         download_scale: Scale factor for the figure resolution when downloading.
         full_html: Whether to wrap the figure in a full html document.
@@ -132,11 +108,11 @@ def plotly_to_html(
     )
 
     fig_id = str(uuid4())[:10]
+    script_id = str(uuid4())[:10]
 
-    fig = fig.update_layout(
-        width=default_width,
-        height=default_height,
-    )
+    layout = cast(go.Layout, fig.layout).to_plotly_json()
+    width = layout.get("width") or 720
+    height = layout.get("height") or 480
 
     # Render the figure to html.
     fig_html: str = fig.to_html(
@@ -147,65 +123,66 @@ def plotly_to_html(
             "toImageButtonOptions": {
                 "format": download_format,
                 "filename": name,
-                "height": default_height,
-                "width": default_width,
+                "height": height,
+                "width": width,
                 "scale": download_scale,
             },
         },
     )
 
     res_html = fig_html
-    if resizing is not None:
-        sorted_sizing = dict(
-            sorted(
-                resizing.items(),
-                key=lambda item: item[1] if item[1] is not None else float("inf"),
-            )
-        )
-
+    if responsive:
         script_attrs = {
             "plotly-js-url": plotly_js_url,
             "fig-id": fig_id,
-            "fig-width": default_width,
-            "fig-height": default_height,
-            **{
-                f"fig-bp-{prop}-{i}": value if value is not None else "inf"
-                for i, (mode, width) in enumerate(sorted_sizing.items())
-                for prop, value in [("mode", mode), ("width", width)]
-            },
+            "fig-width": width,
+            "fig-height": height,
+            "min-width": min_width,
+            "max-width": max_width,
+            "max-height": max_height,
         }
 
         script_attr_str = " ".join(
             f'{attr}="{value}"' for attr, value in script_attrs.items()
         )
 
-        res_html = f"""
-        <div class="plotly-responsive-container" style="width: 100%;">
-            <script
-                type="module"
-                {_ind(script_attr_str, 8)}
-            >
-                {_ind(plotly_responsive_js, 8)}
-                {_ind(fig_html, 8)}
-            </script>
-        </div>
-        """
-
-    return (
-        f"""
-            <!doctype html>
-            <html>
-                <head>
-                    <title>{name}</title>
-                </head>
-                <body>
-                    {_ind(res_html, 8)}
-                </body>
-            </html>
+        res_html = dedent(
+            f"""
+            <div class="plotly-responsive-container" style="width: 100%;">
+                <script src="{plotly_js_url}"></script>
+                {_ind(fig_html, 16)}
+                <script
+                    id="{script_id}"
+                    {_ind(script_attr_str, 20)}
+                >
+                    const scriptID = "{script_id}";
+                    {_ind(plotly_responsive_js, 20)}
+                </script>
+            </div>
             """
+        )
+
+    html = dedent(
+        f"""
+        <!doctype html>
+        <html>
+            <head>
+                <title>{name}</title>
+            </head>
+            <body>
+                {_ind(res_html, 16)}
+            </body>
+            </html>
+         """
         if full_html
         else res_html
     )
+
+    if write_to is not None:
+        with open(write_to, "w") if isinstance(write_to, Path | str) else write_to as f:
+            f.write(html)
+
+    return html
 
 
 P = ParamSpec("P")
--- src/py_research/plots/responsive_plotly.js
+++ src/py_research/plots/responsive_plotly.js
@@ -1,67 +1,25 @@
-const plotlyJsURL = document.currentScript.getAttribute("plotly-js-url");
+const script = document.getElementById(scriptID); // scriptID is from Python
 
-plotlyModule = await import(plotlyJsURL);
-Plotly = plotlyModule.default;
-
-const figureID = document.currentScript.getAttribute("fig-id");
-const figureWidth = document.currentScript.getAttribute("fig-width");
-const figureHeight = document.currentScript.getAttribute("fig-height");
-const sizing = new Map(
-  [0, 1, 2, 3].map((i) => [
-    document.currentScript.getAttribute(`fig-bp-width-${i}`),
-    document.currentScript.getAttribute(`fig-bp-mode-${i}`),
-  ])
-);
+const figureID = script.getAttribute("fig-id");
+const figureWidth = script.getAttribute("fig-width");
+const figureHeight = script.getAttribute("fig-height");
+const minWidth = script.getAttribute("min-width");
+const maxWidth = script.getAttribute("max-width");
+const maxHeight = script.getAttribute("max-height");
 
 const aspect_ratio = figureWidth / figureHeight;
 
-const get_matching_mode = (width) => {
-  matching = Array(sizing.keys()).filter((key) =>
-    key == "inf" ? true : width <= key
-  );
-  return matching.length > 0 ? sizing.get(matching[matching.length - 1]) : null;
-};
-
-let scalingMode = false;
-
-const activateScaling = () => {
-  const figure = document.getElementById(figureID);
-
-  virtualWidth = figure.clientWidth;
-  virtualHeight = figure.clientHeight;
-
-  figure.style.width = "100%";
-  figure.removeAttribute("height");
-
-  plotContainer = figure.getElementsByClassName("svg-container")[0];
-  plotContainer.style.width = "100%";
-  plotContainer.removeAttribute("height");
-
-  viewBox = `0 0 ${virtualWidth} ${virtualHeight}`;
-
-  const svgs = plotContainer.getElementsByTagName("svg");
-
-  for (let svg of svgs) {
-    svg.setAttribute("viewBox", viewBox);
-    plotContainer.removeAttribute("width");
-    plotContainer.removeAttribute("height");
-  }
-
-  scalingMode = true;
-};
+const figure = document.getElementById(figureID);
 
 const resize = (containerWidth) => {
-  const mode = get_matching_mode(containerWidth);
-  if ((mode == "scale-image") & !scalingMode) {
-    activateScaling();
-  } else if (mode == "stretch-width") {
-    Plotly.relayout(figure, { width: containerWidth });
-  } else if (mode == "scale-layout") {
-    Plotly.relayout(figure, {
-      width: containerWidth,
-      height: containerWidth / aspect_ratio,
-    });
-  }
+  targetWidth = Math.min(maxWidth, Math.max(minWidth, containerWidth));
+  optimalHeight = containerWidth / aspect_ratio;
+  targetHeight = Math.min(maxHeight, optimalHeight);
+
+  Plotly.relayout(figure, {
+    width: targetWidth,
+    height: targetHeight,
+  });
 };
 
 const resizeObserver = new ResizeObserver((entries) => {
@@ -81,7 +39,9 @@ const resizeObserver = new ResizeObserver((entries) => {
 const figureContainer = figure.parentElement;
 
 window.onload = () => {
-  resize(figureContainer.clientWidth);
+  window.setTimeout(() => {
+    resize(figureContainer.clientWidth);
+  }, 200);
 };
 
 resizeObserver.observe(figureContainer);
