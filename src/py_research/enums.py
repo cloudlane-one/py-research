91be9733f16428723f24746c4c63dd9ac00513b4
Lorenzo Wormer <l.wormer@pm.me>
2024-02-01 12:04:06 +0100

    🐛(geo): type issues when parsing from yaml

--- src/py_research/enums.py
+++ src/py_research/enums.py
@@ -1,10 +1,17 @@
 """Documentation-friendly enum classes."""
 
+from typing import Self
+
 from strenum import LowercaseStrEnum
 
 
 class StrEnum(LowercaseStrEnum):
     """StrEnum which renders only its value in a string context."""
+
+    @classmethod
+    def parse(cls, value: str) -> Self:
+        """Parse a string into a StrEnum of proper type."""
+        return cls(value)
 
     def __repr__(self) -> str:  # noqa: D105
         return f"'{self.value}'"
