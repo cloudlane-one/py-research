5ceacbe8bde83fb5fe441e8185a3ca9380caaa50
Lorenzo Wormer <l.wormer@pm.me>
2024-02-13 14:52:54 +0100

    🚧(db): integrated new ideas into spec and abstract

--- src/py_research/db/abstract.py
+++ src/py_research/db/abstract.py
@@ -1,11 +1,8 @@
 """Abstract base for connections to relational databases."""
 
 from abc import ABC
-from collections.abc import Hashable
 from dataclasses import dataclass
 from typing import Any, Generic, TypeVar, overload
-
-from yarl import URL
 
 from .spec import (
     S2,
@@ -14,8 +11,13 @@ from .spec import (
     AttrSet,
     Data,
     DataArray,
-    DataPoint,
+    DataBackend,
+    DataBaseSchema,
+    DataNode,
     DataSet,
+    EmptySchema,
+    N,
+    RI_cov,
     S,
     S2_cov,
     S3_cov,
@@ -29,15 +31,18 @@ from .spec import (
 R_cov = TypeVar("R_cov", covariant=True, bound="Ref")
 D_cov = TypeVar("D_cov", covariant=True, bound="Data")
 
+DBS_cov = TypeVar("DBS_cov", covariant=True, bound="DataBaseSchema")
+DBS2_cov = TypeVar("DBS2_cov", covariant=True, bound="DataBaseSchema")
+
 
 @dataclass(frozen=True, kw_only=True)
-class Ref(ABC, Generic[R_cov, D_cov]):
+class Ref(ABC, Generic[N, D_cov]):
     """Readable data connection."""
 
     spec: D_cov
     """Spec of the referenced data."""
 
-    container: R_cov
+    container: "ArrayRef[N, *tuple[Any, ...]]"
     """Container of the referenced data."""
 
     @property
@@ -47,7 +52,7 @@ class Ref(ABC, Generic[R_cov, D_cov]):
 
 
 @dataclass(frozen=True, kw_only=True)
-class Var(Ref[R_cov, D_cov], ABC):
+class Var(Ref[N, D_cov], ABC):
     """Writable data connection."""
 
     @property
@@ -63,89 +68,89 @@ class Var(Ref[R_cov, D_cov], ABC):
 
 @dataclass(frozen=True, kw_only=True)
 class ArrayRef(
-    Ref[R_cov, DataArray[S_cov, V_cov, V2_cov, UI_cov, *TI]],
+    Ref[N, DataArray[S_cov, V_cov, V2_cov, RI_cov, UI_cov, *TI]],
     ABC,
-    Generic[R_cov, S_cov, V_cov, V2_cov, UI_cov, *TI],
+    Generic[N, S_cov, V_cov, V2_cov, RI_cov, UI_cov, *TI],
 ):
     """Readable connection to a data array."""
 
 
 @dataclass(frozen=True, kw_only=True)
 class ArrayVar(
-    ArrayRef[R_cov, S_cov, V_cov, V2_cov, UI_cov, *TI],
+    ArrayRef[N, S_cov, V_cov, V2_cov, RI_cov, UI_cov, *TI],
     ABC,
 ):
     """Writable connection to a data array."""
 
 
 @dataclass(frozen=True, kw_only=True)
-class PointRef(
-    Ref[R_cov, DataPoint[S_cov, V_cov, S2_cov, S3_cov]],
+class NodeRef(
+    Ref[N, DataNode[S_cov, V_cov, S2_cov, S3_cov]],
     ABC,
-    Generic[R_cov, S_cov, V_cov, S2_cov, S3_cov],
+    Generic[N, S_cov, V_cov, S2_cov, S3_cov],
 ):
     """Readable connection to a data point."""
 
 
 @dataclass(frozen=True, kw_only=True)
-class PointVar(PointRef[R_cov, S_cov, V_cov, S2_cov, S3_cov], ABC):
+class NodeVar(NodeRef[N, S_cov, V_cov, S2_cov, S3_cov], ABC):
     """Writable connection to a data point."""
 
 
 @dataclass(frozen=True, kw_only=True)
 class SetRef(
-    Ref[R_cov, DataSet[S_cov, V_cov, V2_cov, S2_cov, S3_cov, UI_cov, *TI]],
+    Ref[N, DataSet[S_cov, V_cov, V2_cov, S2_cov, S3_cov, RI_cov, UI_cov, *TI]],
     ABC,
-    Generic[R_cov, S_cov, V_cov, V2_cov, S2_cov, S3_cov, UI_cov, *TI],
+    Generic[N, S_cov, V_cov, V2_cov, S2_cov, S3_cov, RI_cov, UI_cov, *TI],
 ):
     """Readable connection to a data set."""
 
 
 @dataclass(frozen=True, kw_only=True)
-class SetVar(SetRef[R_cov, S_cov, V_cov, V2_cov, S2_cov, S3_cov, UI_cov, *TI], ABC):
+class SetVar(SetRef[N, S_cov, V_cov, V2_cov, S2_cov, S3_cov, RI_cov, UI_cov, *TI], ABC):
     """Writable connection to a data set."""
 
 
 @dataclass(frozen=True, kw_only=True)
 class DataSource(
+    DataNode[EmptySchema, V_cov, DBS_cov, DBS2_cov],
     ABC,
-    Generic[S_cov, V_cov, V2_cov, S2_cov, S3_cov, UI_cov, *TI],
+    Generic[N, V_cov, DBS_cov, DBS2_cov],
 ):
     """Readable database connection."""
 
-    url: URL
-    namespace: Hashable | None = None
+    backend: DataBackend[N]
 
     @overload
     def __getitem__(
-        self, key: AttrSet[S_cov, S, Any]
-    ) -> "DataSource[S, Any, Any, Any, Any, Any, Any]": ...
+        self, key: AttrSet[DBS_cov, S, Any]
+    ) -> "DataSource[N, S, Any, Any]": ...
 
     @overload
     def __getitem__(
-        self, key: AttrSet[S2_cov, S2, Any]
-    ) -> "DataSource[Any, S2, Any, Any, Any, Any, Any]": ...
+        self, key: AttrSet[DBS2_cov, S2, Any]
+    ) -> "DataSource[Any, S2, Any, Any]": ...
 
     @overload
-    def __getitem__(self, key: AttrRef[Data[S_cov, V], Any, S_cov]) -> V: ...
+    def __getitem__(self, key: AttrRef[Data[DBS_cov, V], Any, DBS_cov]) -> V: ...
 
     @overload
     def __getitem__(
-        self, key: AttrRef[Data[S2_cov, V], Any, S2_cov] | str
+        self, key: AttrRef[Data[DBS2_cov, V], Any, DBS2_cov] | str
     ) -> V | None: ...
 
     def __getitem__(
         self,
         key: (
-            AttrSet[S_cov | S2_cov, Any, Any]
-            | AttrRef[Data[S_cov | S2_cov, V], Any, S_cov | S2_cov]
+            AttrSet[DBS_cov | DBS2_cov, Any, Any]
+            | AttrRef[Data[DBS_cov | DBS2_cov, V], Any, DBS_cov | DBS2_cov]
             | str
         ),
     ) -> "DataSource | V | None":
-        """Get a column of this table."""
+        """Get a dataset in this database."""
         ...
 
 
 @dataclass(frozen=True, kw_only=True)
-class DataBase(DataSource[S_cov, V_cov, V2_cov, S2_cov, S3_cov, UI_cov, *TI], ABC):
+class DataBase(DataSource[N, V_cov, DBS_cov, DBS2_cov], ABC):
     """Writable database connection."""
--- src/py_research/db/spec.py
+++ src/py_research/db/spec.py
@@ -2,13 +2,28 @@
 
 from collections.abc import Hashable, Iterable
 from dataclasses import dataclass
-from typing import Any, Generic, Self, TypeAlias, TypeVar, TypeVarTuple, cast, overload
+from pathlib import Path
+from typing import (
+    Any,
+    Generic,
+    LiteralString,
+    Protocol,
+    Self,
+    TypeAlias,
+    TypeVar,
+    TypeVarTuple,
+    cast,
+    overload,
+)
+
+from yarl import URL
 
 from py_research.reflect.types import is_subtype
 
 S = TypeVar("S", bound="Schema")
 S2 = TypeVar("S2", bound="Schema")
 S3 = TypeVar("S3", bound="Schema")
+S4 = TypeVar("S4", bound="Schema")
 S_cov = TypeVar("S_cov", covariant=True, bound="Schema")
 S2_cov = TypeVar("S2_cov", covariant=True, bound="Schema")
 S3_cov = TypeVar("S3_cov", covariant=True, bound="Schema")
@@ -22,6 +37,10 @@ V = TypeVar("V")
 V2 = TypeVar("V2")
 V_cov = TypeVar("V_cov", covariant=True)
 V2_cov = TypeVar("V2_cov", covariant=True)
+V_contrav = TypeVar("V_contrav", contravariant=True)
+
+RI = TypeVar("RI", bound="RangeValue")
+RI_cov = TypeVar("RI_cov", covariant=True, bound="RangeValue")
 
 UI = TypeVar("UI", bound=Hashable)
 UI_cov = TypeVar("UI_cov", covariant=True)
@@ -30,7 +49,43 @@ TI = TypeVarTuple("TI")
 
 L_cov = TypeVar("L_cov", bound="ArrayLink", covariant=True)
 
-I_cov = TypeVar("I_cov", covariant=True, bound="DataArray")
+DA_cov = TypeVar("DA_cov", covariant=True, bound="DataArray")
+
+N = TypeVar("N", bound="LiteralString")
+
+
+class RangeValue(Hashable, Protocol[V_contrav]):
+    """Range value protocol."""
+
+    def __add__(self, other: V_contrav) -> Self:
+        """Add two index values."""
+        ...
+
+    def __ge__(self, other: Self) -> bool:
+        """Check if this value is greater than or equal to another."""
+        ...
+
+
+@dataclass(frozen=True)
+class Range(Generic[RI_cov, V_cov]):
+    """Range of values."""
+
+    start: RI_cov
+    """Start of the range."""
+
+    end: RI_cov
+    """End of the range."""
+
+    step: V_cov
+    """Step of the range."""
+
+    def __iter__(self: "Range[RangeValue[V_cov], V_cov]") -> Iterable[RI_cov]:
+        """Iterate over the range."""
+        value = self.start
+
+        while value <= self.end:
+            yield cast(RI_cov, value)
+            value += self.step
 
 
 class Schema(Generic[S_cov, A_cov, S2_cov]):
@@ -56,8 +111,12 @@ class Schema(Generic[S_cov, A_cov, S2_cov]):
     def refs(cls) -> set[type[S2_cov]]:
         """Return all schemas referenced by this one."""
         return {
-            attr.schema for attr in cls.__dict__.values() if isinstance(attr, DataPoint)
+            attr.schema for attr in cls.__dict__.values() if isinstance(attr, DataNode)
         }
+
+
+class EmptySchema(Schema):
+    """Empty schema."""
 
 
 class Attribute(Generic[S_cov]):
@@ -98,7 +157,7 @@ class AttrRef(Generic[A_cov, A2_cov, S_cov]):
     """Type of the value of this attribute."""
 
 
-@dataclass
+@dataclass(frozen=True)
 class AttrSet(Generic[S_cov, S2_cov, A_cov]):
     """Select all attributes in a schema."""
 
@@ -114,7 +173,34 @@ class AttrSet(Generic[S_cov, S2_cov, A_cov]):
         )
 
 
-@dataclass
+class NestedData:
+    """Declare data as nested."""
+
+
+IndexKey: TypeAlias = (
+    AttrRef["Data[S, V]", "Data[S, V]", S] | set[AttrRef["Data[S, V]", "Data[S, V]", S]]
+)
+
+
+@dataclass(kw_only=True, frozen=True)
+class ArrayLink(Generic[S, V, S2, RI, UI]):
+    """Link to a table."""
+
+    attr: AttrRef["Data[S, V]", "Data[S, V]", S]
+    target: "DataArray[S2, Any, V, RI, UI, *tuple[Any, ...]] | NestedData"
+    source_key: IndexKey[S, UI] | None = None
+
+
+@dataclass(kw_only=True, frozen=True)
+class SetLink(ArrayLink[S, V, S2, RI, UI], Generic[S, V, S2, S3, S4, RI, UI]):
+    """Link to a table."""
+
+    attr: AttrRef["DataNode[S, V, S3, S4]", Any, S]
+    target: "DataSet[S2, Any, V, S3, S4, RI, UI, *tuple[Any, ...]] | NestedData"
+    target_key: IndexKey[S3, UI]
+
+
+@dataclass(frozen=True)
 class Data(Attribute[S_cov], Generic[S_cov, V_cov]):
     """Data point within a relational database."""
 
@@ -122,87 +208,74 @@ class Data(Attribute[S_cov], Generic[S_cov, V_cov]):
     """Type of the value of this attribute."""
 
 
-@dataclass
-class DataArray(Data[S_cov, V_cov], Generic[S_cov, V_cov, V2_cov, UI_cov, *TI]):
+@dataclass(frozen=True)
+class DataArray(Data[S_cov, V_cov], Generic[S_cov, V_cov, V2_cov, RI_cov, UI_cov, *TI]):
     """Data array in a relational database."""
 
     item_type: type[V2_cov]
     """Value type of this array's items."""
 
-    full_key_type: type[tuple[*TI]]
+    full_index_type: type[tuple[*TI]]
     """Type of the index of this array.
     Use ``tuple`` for multi-dimensional arrays."
     Add ``| slice`` to index types to make them sliceable.
     """
 
-    partial_key_type: type[UI_cov]
+    partial_index_type: type[UI_cov] | None = None
     """Index type union for partial indexing."""
+
+    range_index_type: type[RI_cov] | None = None
+    """Index type for range indexing."""
 
     default: bool = False
     """Whether this is the default array for data of its spec."""
 
 
-IndexKey: TypeAlias = (
-    AttrRef[Data[S, V], Data[S, V], S] | set[AttrRef[Data[S, V], Data[S, V], S]]
-)
-
-
-@dataclass
-class DataPoint(
+@dataclass(frozen=True)
+class DataNode(
     Data[S_cov, V_cov],
     Generic[S_cov, V_cov, S2_cov, S3_cov],
 ):
     """Data point consisting of multiple data values following a schema."""
 
     schema: type[S2_cov]
-    """Schema of this dataset."""
+    """Schema of this data node."""
 
     partial_schema: type[S3_cov] = cast(type[S3_cov], Schema)
-    """Partial schema of this dataset, if any."""
+    """Partial schema of this data node, if any."""
+
+    links: set[ArrayLink[S2_cov | S3_cov, V_cov, S_cov, Any, Any]] = set()
+    """External sources of linked attributes in this node."""
 
 
-class NestedData:
-    """Declare data as nested."""
-
-
-@dataclass(kw_only=True, frozen=True)
-class ArrayLink(Generic[S, V, UI]):
-    """Link to a table."""
-
-    attr: AttrRef[Data[S, V], Any, S]
-    target: "DataArray[Any, V, Any, UI, *tuple[Any, ...]] | NestedData"
-    source_key: IndexKey[S, UI] | None = None
-
-
-@dataclass(kw_only=True, frozen=True)
-class SetLink(ArrayLink[S, V, UI], Generic[S, V, S2, S3, UI]):
-    """Link to a table."""
-
-    attr: AttrRef[DataPoint[S, V, S2, S3], Any, S]
-    target: "DataSet[Any, V, Any, S2, S3, UI, *tuple[Any, ...]] | NestedData"
-    target_key: IndexKey[S2, UI]
-
-
-@dataclass
+@dataclass(frozen=True)
 class DataSet(
-    DataArray[S_cov, V_cov, V2_cov, UI_cov, *TI],
-    DataPoint[S_cov, V_cov, S2_cov, S3_cov],
-    Generic[S_cov, V_cov, V2_cov, S2_cov, S3_cov, UI_cov, *TI],
+    DataArray[S_cov, V_cov, V2_cov, RI_cov, UI_cov, *TI],
+    DataNode[S_cov, V_cov, S2_cov, S3_cov],
+    Generic[S_cov, V_cov, V2_cov, S2_cov, S3_cov, RI_cov, UI_cov, *TI],
 ):
     """Data set in a relational database."""
-
-    schema: type[S2_cov]
-    """Schema of this dataset."""
-
-    partial_schema: type[S3_cov] = cast(type[S3_cov], Schema)
-    """Partial schema of this dataset, if any."""
 
     index_keys: Iterable[IndexKey[S_cov, UI_cov]] = set()
     """Schema attributes to use as indexes of this dataset."""
 
-    links: set[ArrayLink[S_cov, Any, Any]] = set()
-    """External sources of linked attributes in this table."""
 
-
-class DbSchema(Schema[S_cov, I_cov, S2_cov]):
+class DataBaseSchema(Schema[S_cov, DA_cov, S2_cov]):
     """Base class for database schemas."""
+
+
+@dataclass(frozen=True)
+class DataBackend(Generic[N]):
+    """Data backend."""
+
+    name: N
+    """Name of the backend."""
+
+    location: Path | URL
+    """Location of the backend."""
+
+    mime_type: str | None = None
+    """MIME type of the backend (in case of file)."""
+
+    object_storages: dict[type, Path | URL] = {}
+    """Object storages associated with this backend."""
